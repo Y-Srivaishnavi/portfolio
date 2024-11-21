29e5ede6fa07abd30babd117e2e776b8b26c7cb2
Srivaishnavi Yaddanapudi <127314796+Y-Srivaishnavi@users.noreply.github.com>
2024-11-21 20:53:30 +0530

    Update script.js with correct resource location

--- static/script.js
+++ static/script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Script is running");
     let toggleButton = document.getElementById('toggleDarkMode');
-let body = document.body;
+    let body = document.body;
 
     toggleButton.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
         
         if (body.classList.contains('dark-mode')) {
-            toggleButton.innerHTML = '<img height="20" width="20" src=".\\bright.svg" title="Dark mode"/>';
+            toggleButton.innerHTML = '<img height="20" width="20" src="../static/bright.svg" title="Light mode"/>';
         } else {
-            toggleButton.innerHTML = '<img height="20" width="20" src=".\\moon.svg" title="Dark mode"/>';
+            toggleButton.innerHTML = '<img height="20" width="20" src="../static/moon.svg" title="Dark mode"/>';
         }
     });
 });
